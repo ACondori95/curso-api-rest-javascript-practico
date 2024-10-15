ef4f99c197499a05a05d6b4dbe6e252916476cd6
Alvaro Condori <condorialvaro95@gmail.com>
2024-10-15 20:38:59 -0300

    Clase 12: scrollTop y DRY

--- src/main.js
+++ src/main.js
@@ -8,11 +8,9 @@ const api = axios.create({
   },
 });
 
-async function getTrendingMoviesPreview() {
-  const {data} = await api("trending/movie/day");
-  const movies = data.results;
-
-  trendingMoviesPreviewList.innerHTML = "";
+// Utils
+function createMovies(movies, container) {
+  container.innerHTML = "";
 
   movies.forEach((movie) => {
     const movieContainer = document.createElement("div");
@@ -23,19 +21,16 @@ async function getTrendingMoviesPreview() {
     movieImg.setAttribute("alt", movie.title);
     movieImg.setAttribute(
       "src",
-      `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+      `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
     );
 
     movieContainer.appendChild(movieImg);
-    trendingMoviesPreviewList.appendChild(movieContainer);
+    container.appendChild(movieContainer);
   });
 }
 
-async function getCategoriesPreview() {
-  const {data} = await api("genre/movie/list");
-  const categories = data.genres;
-
-  categoryPreviewList.innerHTML = "";
+function createCategories(categories, container) {
+  container.innerHTML = "";
 
   categories.forEach((category) => {
     const categoryContainer = document.createElement("div");
@@ -51,8 +46,23 @@ async function getCategoriesPreview() {
 
     categoryTitle.appendChild(categoryTitleText);
     categoryContainer.appendChild(categoryTitle);
-    categoryPreviewList.appendChild(categoryContainer);
+    container.appendChild(categoryContainer);
   });
+}
+
+// Llamados a la API
+async function getTrendingMoviesPreview() {
+  const {data} = await api("trending/movie/day");
+  const movies = data.results;
+
+  createMovies(movies, trendingMoviesPreviewList);
+}
+
+async function getCategoriesPreview() {
+  const {data} = await api("genre/movie/list");
+  const categories = data.genres;
+
+  createCategories(categories, categoryPreviewList);
 }
 
 async function getMoviesByCategory(id) {
@@ -63,21 +73,5 @@ async function getMoviesByCategory(id) {
   });
   const movies = data.results;
 
-  genericSection.innerHTML = "";
-
-  movies.forEach((movie) => {
-    const movieContainer = document.createElement("div");
-    movieContainer.classList.add("movie-container");
-
-    const movieImg = document.createElement("img");
-    movieImg.classList.add("movie-img");
-    movieImg.setAttribute("alt", movie.title);
-    movieImg.setAttribute(
-      "src",
-      `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-    );
-
-    movieContainer.appendChild(movieImg);
-    genericSection.appendChild(movieContainer);
-  });
+  createMovies(movies, genericSection);
 }
